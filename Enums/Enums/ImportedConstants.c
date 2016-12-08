30a6b09074d3e57f3f182475f37d1ebdb17d8688
cmurray <chris.murray.az@gmail.com>
2016-12-08 11:28:42 -0700

    Initial Commit

---
+++ Enums/Enums/ImportedConstants.c
@@ -0,0 +1,12 @@
+const char Constant1 = abcd;
+const int16 Constant2 = 14;
+const bool Constant3 = true;
+
+/*	In native C, I would want the following lines:
+
+const char Constant1 = 'abcd';
+const int Constant2 = 14;
+There is no bool in C
+There is no int16 in C without the KCG definitions
+
+*/
